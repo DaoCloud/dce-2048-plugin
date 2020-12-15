9cf643a0280f800bccb7245f94cff18f76016adf
vivian-xu <vivian.zxx53@gmail.com>
2020-12-15 10:39:09 +0800

    feat: 增加通过镜像部署挂载点

--- js/dce-sdk.js
+++ js/dce-sdk.js
@@ -2,4 +2,14 @@ window.addEventListener('message', onHandleSaveMessage, true);
 
 function onHandleSaveMessage(evt) {
   console.log(`获取到来自 DCE ${evt.origin} 的数据`, evt.data);
+
+  if (evt.data.action === 'START_VALIDATE') {
+    window.parent.postMessage({
+      action: "REQUEST_VALIDATE",
+      result: {
+        validateResult: true,
+        data: []
+      },
+    }, evt.origin)
+  }
 }
